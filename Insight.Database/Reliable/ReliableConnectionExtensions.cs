ae9a63381e4be0e774c5e02d9362eaadbfbe2819
Jon Wagner <jonwagner@hotmail.com>
2012-06-11 09:26:03 -0400

    Added ReliableConnection extensions

---
+++ Insight.Database/Reliable/ReliableConnectionExtensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Insight.Database.Reliable
+{
+	/// <summary>
+	/// Extension methods for Reliable Connections.
+	/// </summary>
+	public static class ReliableConnectionExtensions
+	{
+		#region ConnectionStringSettings Extensions
+		/// <summary>
+		/// Creates and returns a new SqlConnection.
+		/// </summary>
+		/// <param name="settings">The ConnectionStringSettings containing the connection string.</param>
+		/// <returns>A closed SqlConnection.</returns>
+		public static ReliableConnection<SqlConnection> ReliableConnection(this ConnectionStringSettings settings)
+		{
+			if (settings == null)
+				throw new ArgumentNullException("settings", "ConnectionStringSettings cannot be null");
+
+			return new ReliableConnection<SqlConnection>(settings.ConnectionString);
+		}
+
+		/// <summary>
+		/// Opens and returns a database connection.
+		/// </summary>
+		/// <param name="settings">The connection string to open and return.</param>
+		/// <returns>The opened connection.</returns>
+		public static ReliableConnection<SqlConnection> ReliableOpen(this ConnectionStringSettings settings)
+		{
+			if (settings == null)
+				throw new ArgumentNullException("settings", "ConnectionStringSettings cannot be null");
+
+			ReliableConnection<SqlConnection> connection = settings.ReliableConnection();
+			connection.Open();
+			return connection;
+		}
+
+		/// <summary>
+		/// Converts the connection to a connection that can be invoked dynamically to return lists of FastExpando.
+		/// </summary>
+		/// <param name="settings">The connection to use.</param>
+		/// <returns>A DynamicConnection using the given connection.</returns>
+		public static dynamic ReliableDynamic(this ConnectionStringSettings settings)
+		{
+			return settings.ReliableConnection().Dynamic();
+		}
+
+		/// <summary>
+		/// Converts the connection to a connection that can be invoked dynamically to return lists of type T.
+		/// </summary>
+		/// <param name="settings">The connection to use.</param>
+		/// <typeparam name="T">The type of object to return from queries.</typeparam>
+		/// <returns>A DynamicConnection using the given connection.</returns>
+		public static dynamic ReliableDynamic<T>(this ConnectionStringSettings settings)
+		{
+			return settings.ReliableConnection().Dynamic<T>();
+		}
+		#endregion
+
+		#region SqlConnectionStringBuilder Extensions
+		/// <summary>
+		/// Creates and returns a new SqlConnection.
+		/// </summary>
+		/// <param name="builder">The SqlConnectionStringBuilder containing the connection string.</param>
+		/// <returns>A closed SqlConnection.</returns>
+		public static ReliableConnection<SqlConnection> ReliableConnection(this SqlConnectionStringBuilder builder)
+		{
+			if (builder == null)
+				throw new ArgumentNullException("builder", "SqlConnectionStringBuilder cannot be null");
+
+			return new ReliableConnection<SqlConnection>(builder.ConnectionString);
+		}
+
+		/// <summary>
+		/// Opens and returns a database connection.
+		/// </summary>
+		/// <param name="builder">The connection string to open and return.</param>
+		/// <returns>The opened connection.</returns>
+		public static ReliableConnection<SqlConnection> ReliableOpen(this SqlConnectionStringBuilder builder)
+		{
+			return builder.ReliableConnection().OpenConnection();
+		}
+
+		/// <summary>
+		/// Converts the connection to a connection that can be invoked dynamically to return lists of FastExpando.
+		/// </summary>
+		/// <param name="builder">The connection to use.</param>
+		/// <returns>A DynamicConnection using the given connection.</returns>
+		public static dynamic ReliableDynamic(this SqlConnectionStringBuilder builder)
+		{
+			return builder.ReliableConnection().Dynamic();
+		}
+
+		/// <summary>
+		/// Converts the connection to a connection that can be invoked dynamically to return lists of type T.
+		/// </summary>
+		/// <param name="builder">The connection to use.</param>
+		/// <typeparam name="T">The type of object to return from queries.</typeparam>
+		/// <returns>A DynamicConnection using the given connection.</returns>
+		public static dynamic ReliableDynamic<T>(this SqlConnectionStringBuilder builder)
+		{
+			return builder.ReliableConnection().Dynamic<T>();
+		}
+		#endregion
+	}
+}
