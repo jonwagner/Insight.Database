f40ea71e392cafef72988c94f511b2dde2e6e163
Jon Wagner <jonwagner@hotmail.com>
2013-07-29 15:33:17 -0400

    Updated start/stop batch files for DB2

--- StartServers.bat
+++ StartServers.bat
@@ -2,3 +2,4 @@ net start OracleServiceXE
 net start OracleXETNSListener
 net start MySql56
 net start postgresql-x64-9.2
+net start db2
--- StopServers.bat
+++ StopServers.bat
@@ -2,3 +2,4 @@ net stop OracleServiceXE
 net stop OracleXETNSListener
 net stop MySql56
 net stop postgresql-x64-9.2
+net stop db2
