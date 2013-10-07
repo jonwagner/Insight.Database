49074d82f12d6425e94c08266a61056151ec50bd
Jon Wagner <jonwagner@hotmail.com>
2013-10-06 20:48:24 -0400

    Added Sybase ASE Provider (Issue #52)

---
+++ Insight.Database.Providers.SybaseAse/AseConnectionStringBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Database.Providers.SybaseAse
+{
+	/// <summary>
+	/// An implementation of DbConnectionStringBuilder for Sybase ASE.
+	/// </summary>
+	public class AseConnectionStringBuilder : DbConnectionStringBuilder
+	{
+	}
+}
