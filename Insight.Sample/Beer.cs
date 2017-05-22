4da9011da4f2162cde7d70f6362f1221cbbe3e1d
Jon Wagner <jonwagner@hotmail.com>
2017-05-22 12:02:19 -0400

    Makes sample code (barely) run again

---
+++ Insight.Sample/Beer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insight.Database.Sample
+{
+	class Beer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Flavor { get; set; }
+		public decimal? OriginalGravity { get; set; }
+		public string Details { get; set; }
+		public int InStock { get; set; }
+		public string Style { get; set; }
+
+		public Beer()
+		{
+		}
+
+		public Beer(string name)
+		{
+			Name = name;
+		}
+	}
+}
