a4e187e70ab57121204ef15db7cc9744cd39183a
Jon Wagner <jonwagner@hotmail.com>
2017-05-21 23:01:14 -0400

    Convert library projects to use netstandard2.0

---
+++ Insight.Database/ReflectionCompatibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace Insight.Database
+{
+#if NO_CLASSIC_REFLECTION
+    /// <summary>
+    /// Compatibility methods for reflection methods that are not implemented in .NET standard.
+    /// </summary>
+    static class ReflectionCompatibility
+	{
+		/// <summary>
+        /// Creates a type from a TypeBuilder.
+        /// </summary>
+        /// <param name="builder">The TypeBuilder.</param>
+        /// <returns>The created type.</returns>
+		public static Type CreateType(this TypeBuilder builder)
+		{
+			return builder.CreateTypeInfo().AsType();
+		}
+	}
+#endif
+}
