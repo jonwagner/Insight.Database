fc28e4bc9e5106add1be0ea259a25ac3eb367609
Jon Wagner <jonwagner@hotmail.com>
2017-08-14 22:04:05 -0400

    Updates docker batch file to handle docker on windows

--- insight-docker/docker-env.bat
+++ insight-docker/docker-env.bat
@@ -5,6 +5,7 @@ FOR /f "tokens=*" %%i IN ('docker-machine env default') DO @%%i
 
 REM these variables are read in BaseTest.cs to override localhost and passwords
 SET INSIGHT_TEST_HOST=%DOCKER_HOST%
+IF "%INSIGHT_TEST_HOST%"=="" SET INSIGHT_TEST_HOST=127.0.0.1
 SET INSIGHT_TEST_PASSWORD=Insight!!!Test
 
 ECHO Insight docker environment variables set
