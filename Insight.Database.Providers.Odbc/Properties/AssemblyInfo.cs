36e258ff336f990e1952a203ec426a4ef455e1ea
Jon Wagner <jonwagner@hotmail.com>
2018-01-31 10:51:59 -0500

    Adds support for Odbc on .net Core 2.0 (preview)

---
+++ Insight.Database.Providers.Odbc/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCopyright("Copyright © Jon Wagner, used by permission")]
+[assembly: CLSCompliant(true)]
