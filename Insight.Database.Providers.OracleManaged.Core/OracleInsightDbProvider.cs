02e886ae80b1ddf69b3dfc1b513b18c3b1dd099c
Jon Wagner <jonwagner@hotmail.com>
2018-03-08 13:18:40 -0500

    Adds Oracle.OracleManaged.Core project

---
+++ Insight.Database.Providers.OracleManaged.Core/OracleInsightDbProvider.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Insight.Database.Providers.OracleManaged
+{
+	/// <summary>
+	/// Implements the Insight provider for Oracle ODP.NET connections.
+	/// </summary>
+	public class OracleInsightDbProvider : InsightDbProvider
+	{
+		/// <summary>
+		/// Regex used to auto-detect cursors in queries.
+		/// </summary>
+		private static Regex _cursorSql = new Regex(@"OPEN\s+[@:](?<cursor>\w+)\s+FOR", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline);
+
+		/// <summary>
+		/// The list of types supported by this provider.
+		/// </summary>
+		private static Type[] _supportedTypes = new Type[]
+		{
+			typeof(OracleConnectionStringBuilder), typeof(OracleConnection), typeof(OracleCommand), typeof(OracleDataReader), typeof(OracleException), typeof(OracleParameter)
+		};
+
+		/// <summary>
+		/// Gets the types of objects that this provider supports.
+		/// Include connectionstrings, connections, commands, and readers.
+		/// </summary>
+		public override IEnumerable<Type> SupportedTypes
+		{
+			get
+			{
+				return _supportedTypes;
+			}
+		}
+
+		/// <summary>
+		/// Registers the Oracle Provider.
+		/// </summary>
+		public static void RegisterProvider()
+		{
+			InsightDbProvider.RegisterProvider(new OracleInsightDbProvider());
+		}
+
+		/// <summary>
+		/// Creates a new DbConnection supported by this provider.
+		/// </summary>
+		/// <returns>A new DbConnection.</returns>
+		public override DbConnection CreateDbConnection()
+		{
+			return new OracleConnection();
+		}
+
+		/// <inheritdoc/>
+		public override void FixupCommand(IDbCommand command)
+		{
+			var cmd = (OracleCommand)command;
+			cmd.BindByName = true;
+		}
+
+		/// <summary>
+		/// Derives the parameter list from a stored procedure command.
+		/// </summary>
+		/// <param name="command">The command to derive.</param>
+		public override void DeriveParametersFromStoredProcedure(IDbCommand command)
+		{
+			OracleCommandBuilder.DeriveParameters(command as OracleCommand);
+		}
+
+		/// <inheritdoc/>
+		public override void DeriveParametersFromSqlText(IDbCommand command)
+		{
+			if (command == null) throw new ArgumentNullException("command");
+
+			base.DeriveParametersFromSqlText(command);
+
+			// detect cursors in the command so we can automatically add the parameters as refcursors
+			var cursors = _cursorSql.Matches(command.CommandText).OfType<Match>().Select(m => m.Groups[1].Value);
+
+			if (cursors.Any())
+			{
+				foreach (var c in command.Parameters.OfType<OracleParameter>().Where(p => cursors.Contains(p.ParameterName)))
+				{
+					c.Direction = ParameterDirection.Output;
+					c.OracleDbType = OracleDbType.RefCursor;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Clones a parameter so that it can be used with another command.
+		/// </summary>
+		/// <param name="command">The command to use.</param>
+		/// <param name="parameter">The parameter to clone.</param>
+		/// <returns>The clone.</returns>
+		public override IDataParameter CloneParameter(IDbCommand command, IDataParameter parameter)
+		{
+			// thank you, oracle
+			OracleParameter p = (OracleParameter)parameter;
+			return (IDataParameter)p.Clone();
+		}
+
+		/// <summary>
+		/// Determines if a parameter is an XML type parameter.
+		/// </summary>
+		/// <param name="command">The related command object.</param>
+		/// <param name="parameter">The parameter to test.</param>
+		/// <returns>True if the parameter is an XML parameter.</returns>
+		public override bool IsXmlParameter(IDbCommand command, IDataParameter parameter)
+		{
+			if (parameter == null) throw new ArgumentNullException("parameter");
+
+			var op = (OracleParameter)parameter;
+			return op.OracleDbType == OracleDbType.XmlType;
+		}
+
+		/// <summary>
+		/// Returns SQL that queries a table for the schema only, no rows.
+		/// </summary>
+		/// <param name="connection">The connection to use.</param>
+		/// <param name="tableName">The name of the table to query.</param>
+		/// <returns>SQL that queries a table for the schema only, no rows.</returns>
+		public override string GetTableSchemaSql(IDbConnection connection, string tableName)
+		{
+			return String.Format(CultureInfo.InvariantCulture, "SELECT * FROM {0} WHERE 1=0", tableName);
+		}
+
+		/// <summary>
+		/// Set up a table-valued parameter to a procedure.
+		/// </summary>
+		/// <param name="command">The command to operate on.</param>
+		/// <param name="parameter">The parameter to set up.</param>
+		/// <param name="list">The list of records.</param>
+		/// <param name="listType">The type of object in the list.</param>
+		public override void SetupTableValuedParameter(IDbCommand command, IDataParameter parameter, IEnumerable list, Type listType)
+		{
+			if (parameter == null) throw new ArgumentNullException("parameter");
+
+			// of the many sad things in Oracle, we have to have an array to send the list to the server.
+
+			// return arrays directly
+			if (list is Array)
+			{
+				parameter.Value = list;
+				return;
+			}
+
+			// this can handle any type that is already a list
+			ICollection ilist = list as ICollection;
+			if (ilist != null)
+			{
+				var array = new object[ilist.Count];
+				ilist.CopyTo(array, 0);
+				parameter.Value = array;
+				return;
+			}
+
+			// enumerate the rest :(
+			parameter.Value = list.Cast<object>().ToArray();
+		}
+
+		/// <inheritdoc/>
+		public override bool IsXmlColumn(IDataReader reader, int index)
+		{
+			if (reader == null) throw new ArgumentNullException("reader");
+
+			return ((OracleDbType)reader.GetSchemaTable().Rows[index]["ProviderType"]) == OracleDbType.XmlType;
+		}
+
+		/// <summary>
+		/// Determines if a database exception is a transient exception and if the operation could be retried.
+		/// </summary>
+		/// <param name="exception">The exception to test.</param>
+		/// <returns>True if the exception is transient.</returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
+		public override bool IsTransientException(Exception exception)
+		{
+			OracleException oracleException = (OracleException)exception;
+
+			// there may be more error codes that we need but there are so many to go through....
+			// http://docs.oracle.com/cd/B19306_01/server.102/b14219.pdf
+			switch (oracleException.Number)
+			{
+				case -6403:					// network address connect timeout
+				case 51:					// timeout waiting for a resource
+				case 1033:					// ORACLE initialization or shutdown in progress
+				case 1034:					// ORACLE not available
+				case 1089:					// immediate shutdown in progress - no operations are permitted
+				case 3113:					// Closed connection
+				case 3135:					// connection lost contact
+				case 12150:					// TNS:unable to send data
+				case 12153:					// TNS:not connected
+				case 12154:					// TNS:could not resolve the connect identifier specified
+				case 12157:					// TNS:internal network communication error
+				case 12161:					// TNS:internal error: partial data received
+				case 12170:					// TNS:connect timeout occurred
+				case 12171:					// TNS:could not resolve connect identifier
+				case 12203:					// TNS:could not connect to destination
+				case 12224:					// TNS:no listener
+				case 12225:					// TNS:destination host unreachable
+				case 12537:					// TNS: connection closed
+				case 12541:					// TNS:no listener
+				case 12543:					// TNS:destination host unreachable
+				case 12545:					// Connection Failed (Generally a network failure - Cannot Reach Host)
+				case 12552:					// TNS: Unable to send break
+				case 12571:					// TNS: packet writer failure
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Insight.Tests.OracleManaged.Core/OracleTests.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insight.Database;
+using Insight.Database.Providers.OracleManaged;
+using Insight.Database.Reliable;
+using NUnit.Framework;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Data;
+using System.Data.Common;
+
+#pragma warning disable 0649
+
+namespace Insight.Tests.OracleManaged
+{
+	/// <summary>
+	/// Oracle-specific tests.
+	/// </summary>
+	[TestFixture]
+	public class OracleTests
+	{
+		private OracleConnectionStringBuilder _connectionStringBuilder;
+		private IDbConnection _connection;
+
+		public class ParentTestData
+		{
+			public int ID;
+			public decimal Dec;
+			public TestData TestData;
+		}
+
+		public class TestData
+		{
+			public int X;
+			public int Z;
+		}
+
+		[OneTimeSetUp]
+		public void SetUpFixture()
+		{
+			_connectionStringBuilder = new OracleConnectionStringBuilder();
+			_connectionStringBuilder.ConnectionString = string.Format("Data Source = (DESCRIPTION=(CONNECT_DATA=(SERVICE_NAME=xe))(ADDRESS=(PROTOCOL=TCP)(HOST={0})(PORT=1521))); User Id = system; Password = oracle",
+				BaseTest.TestHost ?? "localhost");
+			_connection = _connectionStringBuilder.Open();
+		}
+
+		[Test]
+		public void TestExecuteSql()
+		{
+			_connection.ExecuteSql("SELECT 1 as p FROM dual");
+		}
+
+		[Test]
+		public void TestExecuteWithParameters()
+		{
+			var result = _connection.QuerySql<decimal>("SELECT :p as p FROM dual", new { p = 5 });
+
+			Assert.AreEqual(1, result.Count);
+			Assert.AreEqual(5, result[0]);
+		}
+
+		[Test]
+		public void TestExecuteProcedure()
+		{
+			try
+			{
+				_connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OracleTestExecute (i int) IS BEGIN null; END;");
+				var result = _connection.Execute("OracleTestExecute", new { i = 5 });
+			}
+			finally
+			{
+				_connection.ExecuteSql("DROP PROCEDURE OracleTestExecute");
+			}
+		}
+
+		[Test]
+		public void TestExecuteProcedureWithOutputParameter()
+		{
+			try
+			{
+				_connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OracleTestOutput (x int, z out int) IS BEGIN z := x; END;");
+				var output = new TestData() { X = 11, Z = 0 };
+				var result = _connection.Execute("OracleTestOutput", output, outputParameters: output);
+
+				Assert.AreEqual(output.X, output.Z);
+			}
+			finally
+			{
+				_connection.ExecuteSql("DROP PROCEDURE OracleTestOutput");
+			}
+		}
+
+		[Test]
+		public void TestQueryProcedure()
+		{
+			try
+			{
+				_connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OracleTestProc (i int, r out sys_refcursor) IS BEGIN open r for select i as p from dual; END;");
+				var result = _connection.Query<decimal>("OracleTestProc", new { i = 5 });
+				Assert.AreEqual(1, result.Count);
+				Assert.AreEqual(5, result[0]);
+			}
+			finally
+			{
+				_connection.ExecuteSql("DROP PROCEDURE OracleTestProc");
+			}
+		}
+
+		[Test]
+		public void TestDynamicExecute()
+		{
+			try
+			{
+				_connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OracleTestProc (i int, r out sys_refcursor) IS BEGIN open r for select i as p from dual; END;");
+				var result = _connection.Dynamic<decimal>().OracleTestProc(i: 5);
+
+				Assert.AreEqual(1, result.Count);
+				Assert.AreEqual(5, result[0]);
+			}
+			finally
+			{
+				_connection.ExecuteSql("DROP PROCEDURE OracleTestProc");
+			}
+		}
+
+		[Test]
+		public void TestQueryRecordset()
+		{
+			try
+			{
+				_connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OracleTestRecordset (r out sys_refcursor) IS BEGIN open r for select 2 as x, 3 as z from dual; END;");
+				var result = _connection.Query<TestData>("OracleTestRecordset");
+
+				Assert.AreEqual(1, result.Count);
+				Assert.AreEqual(2, result[0].X);
+				Assert.AreEqual(3, result[0].Z);
+			}
+			finally
+			{
+				_connection.ExecuteSql("DROP PROCEDURE OracleTestRecordset");
+			}
+		}
+
+		[Test]
+		public void TestEnumerableValueParameters()
+		{
+			string sql = "SELECT p FROM (SELECT 0 AS p FROM dual UNION SELECT 1 AS p FROM dual UNION SELECT 2 AS p FROM dual) WHERE p IN (:p)";
+
+			for (int i = 0; i < 3; i++)
+			{
+				int[] array = Enumerable.Range(0, i).ToArray();
+				var items = _connection.QuerySql(sql, new { p = array });
+				Assert.IsNotNull(items);
+				Assert.AreEqual(i, items.Count);
+			}
+		}
+
+		[Test]
+		public void TestXmlTypes()
+		{
+			try
+			{
+				_connection.ExecuteSql(@"
+					CREATE OR REPLACE PROCEDURE OracleXmlTableProc (id int, testdata xmltype, r out sys_refcursor)
+					IS
+					BEGIN
+						OPEN r FOR SELECT id as id, testdata as testdata FROM dual;
+					END;");
+
+				var testData = new TestData() { X = 9, Z = 13 };
+				var parentTestData = new ParentTestData() { ID = 1, TestData = testData };
+
+				var results = _connection.Query<ParentTestData, TestData>("OracleXmlTableProc", parentTestData);
+				var resultParent = results[0];
+				Assert.AreEqual(parentTestData.ID, resultParent.ID);
+				Assert.IsNotNull(resultParent.TestData);
+				Assert.AreEqual(parentTestData.TestData.X, resultParent.TestData.X);
+				Assert.AreEqual(parentTestData.TestData.Z, resultParent.TestData.Z);
+			}
+			finally
+			{
+				try { _connection.ExecuteSql("DROP PROCEDURE OracleXmlTableProc"); } catch {}
+			}
+		}
+
+		[Test]
+		public void TestBulkLoad()
+		{
+			try
+			{
+				// NOTE: I have not been able to get the xmltype to bulk copy. It throws "unsupported column datatype".
+				// Insight will store the value of the object as a string in the bulk copy
+				//_connection.ExecuteSql("CREATE TABLE InsightTestData (ID int, Dec Decimal, TestData xmltype)");
+				_connection.ExecuteSql("CREATE TABLE InsightTestData (ID int, Dec Decimal)");
+
+				Assert.Throws<NotImplementedException>(() => _connection.BulkCopy("InsightTestData", new List<int>()));
+			}
+			finally
+			{
+				try { _connection.ExecuteSql("DROP TABLE InsightTestData"); }
+				catch { }
+			}
+		}
+
+		private void TestAutoDetectCursorsInSql()
+		{
+			var results = _connection.QueryResultsSql<decimal, decimal>(@"BEGIN
+					OPEN :r1 FOR Select 1 as p from dual;
+					OPEN :r2 FOR Select 2 as p from dual;
+				END;");
+			Assert.AreEqual(1, results.Set1.First());
+			Assert.AreEqual(2, results.Set2.First());
+		}
+
+		#region Output Parameter Tests
+		class OutputParameters
+		{
+			public int out_foo;
+			public int foo;
+		}
+
+		[Test]
+		public void OutputParameterShouldHonorParameterMappings()
+		{
+			try
+			{
+				using (var connection = _connectionStringBuilder.OpenWithTransaction())
+				{
+					connection.ExecuteSql("CREATE OR REPLACE PROCEDURE OutputParameterMappingTest (out_foo out int) IS BEGIN out_foo := 5; END;");
+
+					ColumnMapping.Parameters.RemovePrefixes("out_");
+
+					var output = new OutputParameters();
+					connection.Execute("OutputParameterMappingTest", outputParameters: output);
+					Assert.AreEqual(0, output.out_foo);
+					Assert.AreEqual(5, output.foo);
+				}
+			}
+			finally
+			{
+				ColumnMapping.All.ResetTransforms();
+			}
+		}
+		#endregion
+
+        [Test]
+        public void TestBindByName()
+        {
+            // if the sql uses the same name twice, we need to set bindbyname to true
+            var sql = @"select 
+                       :p1 p1,
+                        :p1 p1,
+                        :p2 p2
+                        from dual";
+
+            var args = new
+            {
+                p1 = "one",
+                p2 = "two"
+            };
+
+            var result = _connection.QuerySql<dynamic>(sql, args);
+            result = _connection.QuerySql<dynamic>(sql, args);
+        }
+
+		[Test]
+		public void TestBindByNameWithWrappedConnection()
+		{
+			var wrapped = new ReliableConnection((DbConnection)_connection);
+
+			// if the sql uses the same name twice, we need to set bindbyname to true
+			var sql = @"select 
+                       :p1 p1,
+                        :p1 p1,
+                        :p2 p2
+                        from dual";
+
+			var args = new
+			{
+				p1 = "one",
+				p2 = "two"
+			};
+
+			var result = wrapped.QuerySql<dynamic>(sql, args);
+			result = wrapped.QuerySql<dynamic>(sql, args);
+		}
+	}
+}
