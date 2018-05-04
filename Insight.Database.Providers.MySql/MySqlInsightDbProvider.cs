c7d064b19eadc7bc4d7275dabfb241e8ae794055
Jon Wagner <jonwagner@hotmail.com>
2018-05-04 12:55:10 -0400

    Updates mysql to 8.x driver

--- Insight.Database.Providers.MySql/MySqlInsightDbProvider.cs
+++ Insight.Database.Providers.MySql/MySqlInsightDbProvider.cs
@@ -11,126 +11,124 @@ using MySql.Data.MySqlClient;
 
 namespace Insight.Database.Providers.MySql
 {
-	/// <summary>
-	/// Implements the Insight provider for MySql connections.
-	/// </summary>
-	public class MySqlInsightDbProvider : InsightDbProvider
-	{
-		/// <summary>
-		/// The prefix used on parameter names.
-		/// </summary>
-		private static Regex _parameterPrefixRegex = new Regex("^[?@:]", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+    /// <summary>
+    /// Implements the Insight provider for MySql connections.
+    /// </summary>
+    public class MySqlInsightDbProvider : InsightDbProvider
+    {
+        /// <summary>
+        /// The prefix used on parameter names.
+        /// </summary>
+        private static Regex _parameterPrefixRegex = new Regex("^[?@:]", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
 
-		/// <summary>
-		/// The list of types supported by this provider.
-		/// </summary>
-		private static Type[] _supportedTypes = new Type[]
-		{
-			typeof(MySqlConnectionStringBuilder), typeof(MySqlConnection), typeof(MySqlCommand), typeof(MySqlParameter), typeof(MySqlDataReader), typeof(MySqlException)
-		};
+        /// <summary>
+        /// The list of types supported by this provider.
+        /// </summary>
+        private static Type[] _supportedTypes = new Type[]
+        {
+            typeof(MySqlConnectionStringBuilder), typeof(MySqlConnection), typeof(MySqlCommand), typeof(MySqlParameter), typeof(MySqlDataReader), typeof(MySqlException)
+        };
 
-		/// <summary>
-		/// Gets the types of objects that this provider supports.
-		/// Include connectionstrings, connections, commands, and readers.
-		/// </summary>
-		public override IEnumerable<Type> SupportedTypes
-		{
-			get
-			{
-				return _supportedTypes;
-			}
-		}
+        /// <summary>
+        /// Gets the types of objects that this provider supports.
+        /// Include connectionstrings, connections, commands, and readers.
+        /// </summary>
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get
+            {
+                return _supportedTypes;
+            }
+        }
 
-		/// <summary>
-		/// Registers this provider. This is generally not needed, unless you want to force an assembly reference to this provider.
-		/// </summary>
-		public static void RegisterProvider()
-		{
-			InsightDbProvider.RegisterProvider(new MySqlInsightDbProvider());
-		}
+        /// <summary>
+        /// Registers this provider. This is generally not needed, unless you want to force an assembly reference to this provider.
+        /// </summary>
+        public static void RegisterProvider()
+        {
+            InsightDbProvider.RegisterProvider(new MySqlInsightDbProvider());
+        }
 
-		/// <summary>
-		/// Creates a new DbConnection supported by this provider.
-		/// </summary>
-		/// <returns>A new DbConnection.</returns>
-		public override DbConnection CreateDbConnection()
-		{
-			return new MySqlConnection();
-		}
+        /// <summary>
+        /// Creates a new DbConnection supported by this provider.
+        /// </summary>
+        /// <returns>A new DbConnection.</returns>
+        public override DbConnection CreateDbConnection()
+        {
+            return new MySqlConnection();
+        }
 
-		/// <summary>
-		/// Derives the parameter list from a stored procedure command.
-		/// </summary>
-		/// <param name="command">The command to derive.</param>
-		public override void DeriveParametersFromStoredProcedure(IDbCommand command)
-		{
-			if (command == null) throw new ArgumentNullException("command");
+        /// <summary>
+        /// Derives the parameter list from a stored procedure command.
+        /// </summary>
+        /// <param name="command">The command to derive.</param>
+        public override void DeriveParametersFromStoredProcedure(IDbCommand command)
+        {
+            if (command == null) throw new ArgumentNullException("command");
 
-			MySqlCommandBuilder.DeriveParameters(command as MySqlCommand);
+            global::MySql.Data.MySqlClient.MySqlCommandBuilder.DeriveParameters(command as MySqlCommand);
 
-			// remove the @ from any parameters
-			foreach (var p in command.Parameters.OfType<MySqlParameter>())
-				p.ParameterName = _parameterPrefixRegex.Replace(p.ParameterName, String.Empty);
-		}
+            // remove the @ from any parameters
+            foreach (var p in command.Parameters.OfType<MySqlParameter>())
+                p.ParameterName = _parameterPrefixRegex.Replace(p.ParameterName, String.Empty);
+        }
 
-		/// <summary>
-		/// Clones a parameter so that it can be used with another command.
-		/// </summary>
-		/// <param name="command">The command to use.</param>
-		/// <param name="parameter">The parameter to clone.</param>
-		/// <returns>The clone.</returns>
-		public override IDataParameter CloneParameter(IDbCommand command, IDataParameter parameter)
-		{
-			MySqlParameter p = (MySqlParameter)parameter;
+        /// <summary>
+        /// Clones a parameter so that it can be used with another command.
+        /// </summary>
+        /// <param name="command">The command to use.</param>
+        /// <param name="parameter">The parameter to clone.</param>
+        /// <returns>The clone.</returns>
+        public override IDataParameter CloneParameter(IDbCommand command, IDataParameter parameter)
+        {
+            MySqlParameter p = (MySqlParameter)parameter;
 
-#if MYSQL_8
-			return new MySqlParameter(
-				p.ParameterName,
-				p.MySqlDbType,
-				p.Size,
-				p.Direction,
-				p.IsNullable,
-				p.Precision,
-				p.Scale,
-				p.SourceColumn,
-				p.Value
-			);
-#else
-			return (IDataParameter)p.Clone();
-#endif
-		}
+            MySqlParameter clone = new MySqlParameter(
+                p.ParameterName,
+                p.MySqlDbType,
+                p.Size,
+                p.SourceColumn);
 
-		/// <summary>
-		/// Returns SQL that queries a table for the schema only, no rows.
-		/// </summary>
-		/// <param name="connection">The connection to use.</param>
-		/// <param name="tableName">The name of the table to query.</param>
-		/// <returns>SQL that queries a table for the schema only, no rows.</returns>
-		public override string GetTableSchemaSql(IDbConnection connection, string tableName)
-		{
-			return String.Format(CultureInfo.InvariantCulture, "SELECT TOP 0 * FROM {0}", tableName);
-		}
+            clone.Direction = p.Direction;
+            clone.IsNullable = p.IsNullable;
+            clone.Precision = p.Precision;
+            clone.Scale = p.Scale;
+            clone.Value = p.Value;
 
-		/// <summary>
-		/// Determines if a database exception is a transient exception and if the operation could be retried.
-		/// </summary>
-		/// <param name="exception">The exception to test.</param>
-		/// <returns>True if the exception is transient.</returns>
-		public override bool IsTransientException(Exception exception)
-		{
-			MySqlException mex = (MySqlException)exception;
-			switch (mex.Number)
-			{
-				case 1042:		// ER_BAD_HOST_ERROR
-				case 2002:		// CR_CONNECTION_ERROR
-				case 2003:		// CR_CONN_HOST_ERROR
-				case 2006:		// CR_SERVER_GONE_ERROR
-				case 2009:		// CR_WRONG_HOST_INFO
-				case 2013:		// CR_SERVER_LOST
-					return true;
-			}
+            return clone;
+        }
 
-			return false;
-		}
-	}
+        /// <summary>
+        /// Returns SQL that queries a table for the schema only, no rows.
+        /// </summary>
+        /// <param name="connection">The connection to use.</param>
+        /// <param name="tableName">The name of the table to query.</param>
+        /// <returns>SQL that queries a table for the schema only, no rows.</returns>
+        public override string GetTableSchemaSql(IDbConnection connection, string tableName)
+        {
+            return String.Format(CultureInfo.InvariantCulture, "SELECT TOP 0 * FROM {0}", tableName);
+        }
+
+        /// <summary>
+        /// Determines if a database exception is a transient exception and if the operation could be retried.
+        /// </summary>
+        /// <param name="exception">The exception to test.</param>
+        /// <returns>True if the exception is transient.</returns>
+        public override bool IsTransientException(Exception exception)
+        {
+            MySqlException mex = (MySqlException)exception;
+            switch (mex.Number)
+            {
+                case 1042:      // ER_BAD_HOST_ERROR
+                case 2002:      // CR_CONNECTION_ERROR
+                case 2003:      // CR_CONN_HOST_ERROR
+                case 2006:      // CR_SERVER_GONE_ERROR
+                case 2009:      // CR_WRONG_HOST_INFO
+                case 2013:      // CR_SERVER_LOST
+                    return true;
+            }
+
+            return false;
+        }
+    }
 }
