50abd2b1e9241538220fb0e2b2fab6458d8f4cae
Jon Wagner <jonwagner@hotmail.com>
2020-05-25 10:59:58 -0400

    Consolidates hash code calcualtion for schema/column identity

--- Insight.Database.Core/CodeGenerator/SchemaIdentity.cs
+++ Insight.Database.Core/CodeGenerator/SchemaIdentity.cs
@@ -9,68 +9,68 @@ using System.Text;
 
 namespace Insight.Database.CodeGenerator
 {
-	/// <summary>
-	/// An identity for the schema of a reader.
-	/// </summary>
-	class SchemaIdentity : IEquatable<SchemaIdentity>
-	{
-		#region Fields
-		/// <summary>
-		/// The pre-calculated hashcode.
-		/// </summary>
-		private int _hashCode;
+    /// <summary>
+    /// An identity for the schema of a reader.
+    /// </summary>
+    class SchemaIdentity : IEquatable<SchemaIdentity>
+    {
+        #region Fields
+        /// <summary>
+        /// The pre-calculated hashcode.
+        /// </summary>
+        private int _hashCode;
 
-		/// <summary>
-		/// The list of columns in the schema as a Tuple of string + Type + IsNullable + IsReadOnly + IsIdentity.
-		/// </summary>
-		private List<ColumnInfo> _columns;
-		#endregion
+        /// <summary>
+        /// The list of columns in the schema as a Tuple of string + Type + IsNullable + IsReadOnly + IsIdentity.
+        /// </summary>
+        private List<ColumnInfo> _columns;
+        #endregion
 
-		#region Constructors
-		/// <summary>
-		/// Initializes a new instance of the SchemaIdentity class.
-		/// </summary>
-		/// <param name="reader">The reader to construct from.</param>
-		public SchemaIdentity(IDataReader reader)
-		{
-			ReadSchema(reader);
-			CalculateHashCode();
-		}
-		#endregion
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the SchemaIdentity class.
+        /// </summary>
+        /// <param name="reader">The reader to construct from.</param>
+        public SchemaIdentity(IDataReader reader)
+        {
+            ReadSchema(reader);
+            CalculateHashCode();
+        }
+        #endregion
 
-		#region Equality Members
-		/// <summary>
-		/// Returns the hash code for the identity.
-		/// </summary>
-		/// <returns>The hash code for the identity.</returns>
-		public override int GetHashCode()
-		{
-			return _hashCode;
-		}
+        #region Equality Members
+        /// <summary>
+        /// Returns the hash code for the identity.
+        /// </summary>
+        /// <returns>The hash code for the identity.</returns>
+        public override int GetHashCode()
+        {
+            return _hashCode;
+        }
 
-		/// <summary>
-		/// Determines if this is equal to another object.
-		/// </summary>
-		/// <param name="obj">The object to test against.</param>
-		/// <returns>True if the objects are equal.</returns>
-		public override bool Equals(object obj)
-		{
-			return Equals(obj as SchemaIdentity);
-		}
+        /// <summary>
+        /// Determines if this is equal to another object.
+        /// </summary>
+        /// <param name="obj">The object to test against.</param>
+        /// <returns>True if the objects are equal.</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as SchemaIdentity);
+        }
 
-		/// <summary>
-		/// Determines if this is equal to another object.
-		/// </summary>
-		/// <param name="other">The object to test against.</param>
-		/// <returns>True if the objects are equal.</returns>
-		public bool Equals(SchemaIdentity other)
-		{
-			if (other == null)
-				return false;
+        /// <summary>
+        /// Determines if this is equal to another object.
+        /// </summary>
+        /// <param name="other">The object to test against.</param>
+        /// <returns>True if the objects are equal.</returns>
+        public bool Equals(SchemaIdentity other)
+        {
+            if (other == null)
+                return false;
 
-			int columnCount = _columns.Count;
-			if (columnCount != other._columns.Count)
-				return false;
+            int columnCount = _columns.Count;
+            if (columnCount != other._columns.Count)
+                return false;
 
             for (int i = 0; i < columnCount; i++)
             {
@@ -78,50 +78,39 @@ namespace Insight.Database.CodeGenerator
                     return false;
             }
 
-			return true;
-		}
-		#endregion
+            return true;
+        }
+        #endregion
 
-		#region Initialization
-		/// <summary>
-		/// Reads the schema from a data reader.
-		/// </summary>
-		/// <param name="reader">The reader to process.</param>
-		private void ReadSchema(IDataReader reader)
-		{
-			_columns = ColumnInfo.FromDataReader(reader);
-		}
+        #region Initialization
+        /// <summary>
+        /// Reads the schema from a data reader.
+        /// </summary>
+        /// <param name="reader">The reader to process.</param>
+        private void ReadSchema(IDataReader reader)
+        {
+            _columns = ColumnInfo.FromDataReader(reader);
+        }
 
-		/// <summary>
-		/// Calculates the hash code for the identity.
-		/// </summary>
-		private void CalculateHashCode()
-		{
-			// we know that we are going to store this in a hashtable, so pre-calculate the hashcode
-			unchecked
-			{
-				// base the hashcode on the column names and types
-				_hashCode = 17;
+        /// <summary>
+        /// Calculates the hash code for the identity.
+        /// </summary>
+        private void CalculateHashCode()
+        {
+            // we know that we are going to store this in a hashtable, so pre-calculate the hashcode
+            unchecked
+            {
+                // base the hashcode on the column names and types
+                _hashCode = 17;
 
-				foreach (var column in _columns)
-				{
-					// update the hash code for the name and type
-					_hashCode *= 23;
-					_hashCode += column.Name.GetHashCode();
-					_hashCode *= 23;
-					_hashCode += column.DataType.GetHashCode();
-					_hashCode *= 23;
-					if (column.IsNullable)
-						_hashCode++;
-					_hashCode *= 23;
-					if (column.IsReadOnly)
-						_hashCode++;
-					_hashCode *= 23;
-					if (column.IsIdentity)
-						_hashCode++;
-				}
-			}
-		}
-		#endregion
-	}
+                foreach (var column in _columns)
+                {
+                    // update the hash code for the name and type
+                    _hashCode *= 23;
+                    _hashCode += column.GetHashCode();
+                }
+            }
+        }
+        #endregion
+    }
 }
