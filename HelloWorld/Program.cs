25f58e26fe16657c9c31aff13121be21acf43648
Jon Wagner <jonwagner@hotmail.com>
2020-11-07 10:57:26 -0500

    Adds HelloWorld project to test startup code

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.SqlClient;
+using Insight.Database;
+
+namespace HelloWorld
+{
+    class Program
+    {
+		public static readonly string connectionString = "Data Source = .; Initial Catalog = InsightDbTests; Integrated Security = false; User ID = sa; Password = Insight!!!Test";
+        private static SqlConnectionStringBuilder Database = new SqlConnectionStringBuilder(connectionString);
+
+        static void Main(string[] args)
+        {
+            using (var c = Database.Open())
+			{
+				Console.WriteLine(c.ExecuteScalarSql<String>("SELECT 'Hello, World.'"));
+			}
+        }
+    }
+}
