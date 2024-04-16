e4c96355a97fa39d34c1e571d7e372c7e6845464
Jon Wagner <jonwagner@hotmail.com>
2024-04-16 08:34:44 -0400

    Test case for Issue #498

---
+++ Insight.Tests.MsSqlClient/SqlServerTests.cs
@@ -0,0 +1,27 @@
+﻿using Insight.Database;
+using Insight.Tests.MsSqlClient.Cases;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System.Data;
+using System.Linq;
+
+namespace Insight.Tests.MsSqlClient
+{
+	[TestFixture]
+	public partial class SqlServerTests : MsSqlClientBaseTest
+	{
+		public class SprocMetadata
+		{
+			public string Name;
+		}
+
+		[Test]
+		public void TestSysProcs()
+		{
+			// Issue #498
+			var results = Connection().QuerySql<SprocMetadata>("SELECT * FROM sys.objects");
+			ClassicAssert.Greater(results.Count, 0);
+			ClassicAssert.IsNotNull(results[0].Name);
+		}
+	}
+}
