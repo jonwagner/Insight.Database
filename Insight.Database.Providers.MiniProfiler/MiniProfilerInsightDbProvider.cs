cf0a737b9fcc829a2ce14649bff49bc07d518762
Jon Wagner <jonwagner@hotmail.com>
2025-01-08 13:36:02 -0500

    Added back support for MiniProfiler

---
+++ Insight.Database.Providers.MiniProfiler/MiniProfilerInsightDbProvider.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Profiling.Data;
+
+namespace Insight.Database.Providers.MiniProfiler
+{
+	/// <summary>
+	/// Implements the Insight provider for MiniProfiler connections.
+	/// </summary>
+	public class MiniProfilerInsightDbProvider : WrappedInsightDbProvider
+	{
+		/// <summary>
+		/// Gets the types of objects that this provider supports.
+		/// Include connectionstrings, connections, commands, and readers.
+		/// </summary>
+		public override IEnumerable<Type> SupportedTypes
+		{
+			get
+			{
+				return new Type[] { typeof(ProfiledDbConnection), typeof(ProfiledDbCommand) };
+			}
+		}
+
+		/// <summary>
+		/// Registers this provider. This is generally not needed, unless you want to force an assembly reference to this provider.
+		/// </summary>
+		public static void RegisterProvider()
+		{
+			InsightDbProvider.RegisterProvider(new MiniProfilerInsightDbProvider());
+		}
+
+		/// <inheritdoc/>
+		public override IDbConnection CloneDbConnection(IDbConnection connection)
+		{
+			if (connection == null) throw new ArgumentNullException("connection");
+
+			// clone the inner connection
+			var innerConnection = GetInnerConnection(connection);
+			var innerProvider = InsightDbProvider.For(innerConnection);
+			var clonedInnerConnection = (DbConnection)innerProvider.CloneDbConnection(innerConnection);
+
+			var profiledConnection = (ProfiledDbConnection)connection;
+			return new ProfiledDbConnection(clonedInnerConnection, profiledConnection.Profiler);
+		}
+
+		/// <summary>
+		/// Unwraps the given connection and returns the inner connection.
+		/// </summary>
+		/// <param name="connection">The outer connection.</param>
+		/// <returns>The inner connection.</returns>
+		public override IDbConnection GetInnerConnection(IDbConnection connection)
+		{
+			ProfiledDbConnection profiledConnection = (ProfiledDbConnection)connection;
+			return profiledConnection.WrappedConnection;
+		}
+
+		/// <summary>
+		/// Unwraps the given command and returns the inner command.
+		/// </summary>
+		/// <param name="command">The outer command.</param>
+		/// <returns>The inner command.</returns>
+		public override IDbCommand GetInnerCommand(IDbCommand command)
+		{
+			if (command == null) throw new ArgumentNullException("command");
+
+			ProfiledDbCommand profiledCommand = (ProfiledDbCommand)command;
+			return profiledCommand.WrappedCommand;
+		}
+	}
+}
---
+++ Insight.Tests.MiniProfiler/MiniProfilerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Insight.Database;
+using Insight.Database.Providers.MiniProfiler;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Data;
+
+namespace Insight.Tests
+{
+	/// <summary>
+	/// Tests that the mini-profiler connection wrapper does not interfere with SQL parameter detection.
+	/// </summary>
+	[TestFixture]
+	class MiniProfilerTests : BaseTest
+	{
+		[OneTimeSetUp]
+		public static void OneTimeSetup()
+		{
+			TestSetup.CreateTestDatabase();
+		}
+
+		[Test]
+		public void TestProfiledSqlQuery()
+		{
+			var profiled = new ProfiledDbConnection((DbConnection)Connection(), MiniProfiler.Current);
+			var result = profiled.QuerySql<int>("SELECT @p --MiniProfiler", new { p = 1 }).First();
+
+			ClassicAssert.AreEqual((int)1, result);
+		}
+
+		/// <summary>
+		/// Make sure that a profiled connection still can auto-detect the parameters.
+		/// </summary>
+		[Test]
+		public void TestProfiledStoredProcWithParameters()
+		{
+			using (var connection = ConnectionWithTransaction())
+			{
+				connection.ExecuteSql("CREATE PROC InsightTestProcMiniProfiler (@Value int = 5) AS SELECT Value=@Value");
+
+				var profiled = new ProfiledDbConnection((DbConnection)connection, MiniProfiler.Current);
+				var result = profiled.Query<int>("InsightTestProcMiniProfiler", new { Value = 1 }).First();
+
+				ClassicAssert.AreEqual((int)1, result);
+			}
+		}
+	}
+}
